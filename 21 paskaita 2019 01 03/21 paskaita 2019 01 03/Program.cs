6aea8822692bfee1c698e4f43852b0184e00b9e4
an2riuz <an2riuz@gmail.com>
2019-01-03 18:56:57 +0200

    Pradzia.

---
+++ 21 paskaita 2019 01 03/21 paskaita 2019 01 03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_paskaita_2019_01_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sarasas = new List<int>{1,2,3,4,5,6,7,8,9,10 };
+
+
+        }
+    }
+}
