cf6de040d6751638e34a8324d8fe59739ab19a55
an2riuz <an2riuz@gmail.com>
2019-01-03 21:47:23 +0200

    Pabaiga.

--- 21 paskaita 2019 01 03/21 paskaita 2019 01 03/Program.cs
+++ 21 paskaita 2019 01 03/21 paskaita 2019 01 03/Program.cs
@@ -10,8 +10,60 @@ namespace _21_paskaita_2019_01_03
     {
         static void Main(string[] args)
         {
-            List<int> sarasas = new List<int>{1,2,3,4,5,6,7,8,9,10 };
+            List<int> sarasas = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+            var result = from skaicius in sarasas
+                         where skaicius % 2 == 1
+                         select skaicius;
+
+            foreach (int skaicius in result)
+            {
+                Console.WriteLine("Nelyginis skaicius: " + skaicius);
+            }
+
+            var result1 = from skaicius in sarasas
+                          where skaicius % 5 == 0
+                          select skaicius;
+
+            foreach (int skaicius in result1)
+            {
+                Console.WriteLine("Dalijasi is 5: " + skaicius);
+            }
+
+            var result2 = sarasas.Where(skaicius => skaicius.Equals(7));
+
+            foreach (int skaicius in result2)
+            {
+                Console.WriteLine("Ar yra septyni: " + skaicius);
+            }
+
+            var result3 = from skaicius in sarasas
+                          where skaicius % 2 == 0
+                          select skaicius;
+
+            foreach (int skaicius in result3)
+            {
+                
+            }
+
+            var result5 = from skaicius in sarasas
+                          where skaicius > 4 && skaicius < 8
+                          select skaicius * skaicius;
+
+            foreach (int skaicius in result5)
+            {
+                Console.WriteLine("Skaiciai kvadratu: " + skaicius);
+            }
+
+            var result6 = from skaicius in sarasas
+                          where skaicius > 4 && skaicius < 8
+                          select skaicius;
+
+            Console.WriteLine("Suma: " + result6.Sum());
+            Console.WriteLine("Vidurkis: " + result6.Average());
+
+
+            Console.ReadKey();
 
         }
     }
---
+++ 21 paskaita 2019 01 03/21 paskaita 2019 01 03/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_paskaita_2019_01_03
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float AverageMark { get; set; }
+        public bool IsGettingTuition { get; set; }
+
+        public Student(int Id, string Name, int Age, float AverageMark, bool IsGettingTuition)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.AverageMark = AverageMark;
+            this.IsGettingTuition = IsGettingTuition;
+        }
+    }
+}
